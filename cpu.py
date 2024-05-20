fbdb4a9ec76c2bec138ea45aa32ab9891a54b25f
Kyle Chapman <kyleichapman@gmail.com>
2024-05-20 14:35:53 +0200

    Support program file reading

--- cpu.py
+++ cpu.py
@@ -1,4 +1,9 @@
+import sys
+
 from typing import Tuple, List
+
+# True to print out debug information, False otherwise
+DEBUG = False
 
 # Size of the RAM in bytes
 MEMORY_SIZE = 255
@@ -22,26 +27,31 @@ INSTRUCTIONS = {
 	"JUMP": 10,                # Jump to address, args: addr (size = 2)
 	"JUMP_IF_ZERO": 11,        # Jump to address if register is zero, args: reg, addr (size = 3)
 	"JUMP_IF_NOT_ZERO": 12,    # Jump to address if register is not zero, args: reg, addr (size = 3)
+	"TITLE": 13,               # Title of the program, args: prog_name (size = 2)
 
-	"COUNT": 13,               # Upper limit of the instruction set
+	"COUNT": 14,               # Upper limit of the instruction set
 }
 
-# Number of arguments for each instruction
+# Number of arguments for each instruction, in form INSTRUCTION_CODE: NUM_ARGS
 NUM_ARGS = {
-	"EXIT": 0,
-	"PUT": 2,
-	"LOAD": 2,
-	"STORE": 2,
-	"ARITHMETIC_PLUS": 3,
-	"ARITHMETIC_MULTIPLY": 3,
-	"ARITHMETIC_NEGATE": 2,
-	"LOGICAL_AND": 3,
-	"LOGICAL_OR": 3,
-	"LOGICAL_NOT": 2,
-	"JUMP": 1,
-	"JUMP_IF_ZERO": 2,
-	"JUMP_IF_NOT_ZERO": 2
+	255: 1,   # EXIT
+	1: 2,     # PUT
+	2: 2,     # LOAD
+	3: 2,     # STORE
+	4: 3,     # ARITHMETIC_PLUS
+	5: 3,     # ARITHMETIC_MULTIPLY
+	6: 2,     # ARITHMETIC_NEGATE
+	7: 3,     # LOGICAL_AND
+	8: 3,     # LOGICAL_OR
+	9: 2,     # LOGICAL_NOT
+	10: 1,    # JUMP
+	11: 2,    # JUMP_IF_ZERO
+	12: 2,    # JUMP_IF_NOT_ZERO
+	13: 1     # TITLE
 }
+
+# List of programs that we have loaded into memory
+PROGRAMS = []
 
 # Position of the currently executing instruction
 prog_ctr = 0
@@ -60,9 +70,9 @@ def to_instruction_key(value: int | str) -> str:
 		return next(k for k, v in INSTRUCTIONS.items() if v == value)
 
 	if isinstance(value, str):
-		converted = value.to_upper()
+		converted = value.upper()
 
-		if converted in ["EXIT", "PUT", "LOAD", "STORE"]:
+		if converted in ["EXIT", "PUT", "LOAD", "STORE", "TITLE"]:
 			# No name changes are necessary
 			return converted
 		elif converted == "ADD":
@@ -85,6 +95,76 @@ def to_instruction_key(value: int | str) -> str:
 			return "JUMP_IF_NOT_ZERO"
 
 	raise ValueError(f"Invalid instruction value: {value}")
+
+
+def read_program_from_file(file, start_addr = 0):
+	"""
+	Reads a program from a file and loads it into memory.
+	"""
+	global MEMORY
+
+	prog_title = ""
+	last_instr_end_addr = start_addr
+
+	with open(file, "r") as f:
+		lines = f.readlines()
+
+		for i, line in enumerate(lines):
+			if line.startswith("TITLE"):
+				prog_title = line[6:].strip()
+				continue
+
+			# Remove comments and whitespace
+			line = line.split("#")[0].strip()
+
+			# Remove any commas
+			line = line.replace(",", "")
+
+			# Skip empty lines
+			if not line:
+				continue
+
+			# Split the line into tokens
+			tokens = line.split(" ")
+
+			# Get the instruction key
+			instruction_key = to_instruction_key(tokens[0])
+
+			# Get the instruction value
+			instruction_value = INSTRUCTIONS[instruction_key]
+
+			# Get the arguments
+			args = [int(arg) for arg in tokens[1:]]
+
+			# Truncate the arguments
+			if len(args) > NUM_ARGS[instruction_value]:
+				args = args[:NUM_ARGS[instruction_value]]
+
+			# Append zeros to the arguments
+			if len(args) < NUM_ARGS[instruction_value]:
+				args += [0] * (NUM_ARGS[instruction_value] - len(args))
+
+			if i == 0:
+				curr_idx = start_addr
+			else:
+				curr_idx = last_instr_end_addr
+
+			# Update the end address
+			last_instr_end_addr = curr_idx + 1 + len(args)
+
+			# Check if we have enough memory to write the instruction
+			if last_instr_end_addr >= MEMORY_SIZE:
+				raise ValueError("Out of memory!")
+
+			if DEBUG:
+				print(f"Writing instruction: {instruction_key} {args} at address {curr_idx}")
+
+			# Write instruction to memory
+			MEMORY[curr_idx] = instruction_value
+			MEMORY[curr_idx + 1:last_instr_end_addr] = args
+
+	# Return the address that this program ends at (and title if available)
+	return last_instr_end_addr, prog_title
 
 
 def decode_instruction() -> Tuple[int, List[int]]:
@@ -110,7 +190,7 @@ def execute_instruction(op_code: int, arguments: List[int]):
 	"""
 	Executes the given instruction with its arguments.
 	"""
-	global prog_ctr
+	global prog_ctr, MEMORY
 
 	if op_code == INSTRUCTIONS["PUT"]:
 		value, addr = arguments
@@ -163,23 +243,78 @@ def cpu_cycle():
 	execute_instruction(op_code, args)
 
 
-def run_computer():
+def run_program(program):
 	"""
-	Runs the computer.
+	Runs the given program, starting at its position in memory.
 	"""
 	global prog_ctr
+	prog_ctr = program["start_addr"]
+	ret_value = None
 
-	# Keep running until we have an exit instruction
 	while True:
+		# No more memory to read from
+		if prog_ctr >= MEMORY_SIZE:
+			break
+
+		# Do not perform cycle when there is no instruction
+		if MEMORY[prog_ctr] == 0:
+			prog_ctr += 1
+			continue
+
 		cpu_cycle()
 
+		if DEBUG:
+			print(f"\nAT INDEX ({prog_ctr})\n------------")
+			print(MEMORY)
+
+		# Keep running until we have an exit instruction
 		if MEMORY[prog_ctr] == INSTRUCTIONS["EXIT"]:
+			final_addr = MEMORY[prog_ctr + 1]
+			ret_value = None if MEMORY[final_addr] <= 0 else MEMORY[final_addr]
 			break
+
+	if ret_value is not None:
+		print(("\n" if DEBUG else "") + "Output: " + str(ret_value))
+
+
+def load_program(path: str, start_addr: int = 0) -> int:
+	"""
+	Loads a program from the given path and adds it to the list of programs.
+
+	@returns End address of the program + 1, so the next program can start at
+	the returned address.
+	"""
+	global PROGRAMS
+
+	end_addr, name = read_program_from_file(path, start_addr)
+
+	if not name:
+		name = "Unnamed Program"
+
+	PROGRAMS.append({
+		"name": name,
+		"start_addr": start_addr,
+		"end_addr": end_addr
+	})
+
+	if DEBUG:
+		print(f"\nProgram loaded into memory from address {start_addr} to {end_addr - 1}.")
+
+	return end_addr
 
 
 if __name__ == "__main__":
 	"""
 	Main entry point.
 	"""
+	if len(sys.argv) > 1:
+		DEBUG = True if sys.argv[1] == "1" else False
 
-	run_computer()
+	start_addr = 0
+	load_program("programs/add-two-numbers.txt", start_addr)
+
+	if DEBUG:
+		print("\nINITIAL\n------------")
+		print(MEMORY)
+
+	run_program(PROGRAMS[0])
